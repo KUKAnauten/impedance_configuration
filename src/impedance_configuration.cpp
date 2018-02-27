eca1a7133fa169f94801941189fe04d08ad51b17
Niklas Schaefer <niklasjoel@gmx.net>
2018-02-27 15:04:54 +0100

    toggle impedance

--- src/impedance_configuration.cpp
+++ src/impedance_configuration.cpp
@@ -36,6 +36,7 @@
 
 #include <iimoveit/robot_interface.h>
 #include <iiwa_ros.h>
+#include <iiwa_ros/conversions.h>
 #include <iiwa_msgs/CartesianQuantity.h>
 #include <iiwa_msgs/JointQuantity.h>
 
@@ -46,10 +47,40 @@ class ImpedanceConfiguration : public iimoveit::RobotInterface {
 public:
   ImpedanceConfiguration(ros::NodeHandle* node_handle, const std::string& planning_group, const std::string& base_frame)
       : RobotInterface(node_handle, planning_group, base_frame) {	
+
+		iiwa_initial_joint_positions_.joint_names.resize(7);
+		iiwa_initial_joint_positions_.joint_names = RobotInterface::getJointNames();
+		iiwa_initial_joint_positions_.points.resize(1);
+		iiwa_initial_joint_positions_.points[0].positions.resize(7);
+		iiwa_initial_joint_positions_.points[0].positions[0] = 3.1416/180.0 * -44.2626;
+		iiwa_initial_joint_positions_.points[0].positions[1] = 3.1416/180.0 * (16.7988 - 90.0);
+		iiwa_initial_joint_positions_.points[0].positions[2] = 3.1416/180.0 * -1.0 * -38.9998;
+		iiwa_initial_joint_positions_.points[0].positions[3] = 3.1416/180.0 * -67.7357;
+		iiwa_initial_joint_positions_.points[0].positions[4] = 3.1416/180.0 * (-1.0 * 61.3977 + 90.0); 
+		iiwa_initial_joint_positions_.points[0].positions[5] = 3.1416/180.0 * -11.0314; 
+		iiwa_initial_joint_positions_.points[0].positions[6] = 3.1416/180.0 * 0.0;
+
+		iiwa_ros_object_.init();
+		//smart_servo_service_caller = iiwa_ros_object_.getSmartServoService();
   }
 
-private:
+	void moveToInitialJointPositions() {
+		planAndMove(iiwa_initial_joint_positions_.points[0].positions, std::string("initial joint positions"));
+	}
 
+	bool setJointImpedanceMode(const iiwa_msgs::JointQuantity& joint_stiffness, const iiwa_msgs::JointQuantity& joint_damping) {
+		//return smart_servo_service_caller.setJointImpedanceMode(joint_stiffness, joint_damping);
+		return iiwa_ros_object_.getSmartServoService().setJointImpedanceMode(joint_stiffness, joint_damping);
+	}
+
+	bool setPositionControlMode() {
+		return iiwa_ros_object_.getSmartServoService().setPositionControlMode();
+	} 	
+
+private:
+	iiwa_ros::iiwaRos iiwa_ros_object_;
+	//SmartServoService smart_servo_service_caller;
+	trajectory_msgs::JointTrajectory iiwa_initial_joint_positions_;
 	
 };
 } // namespace impedance_configuration
@@ -61,6 +92,25 @@ int main(int argc, char **argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
+	iiwa_msgs::JointQuantity joint_stiffness1 = iiwa_ros::jointQuantityFromDouble(250, 250, 250, 250, 250, 250, 250);
+	iiwa_msgs::JointQuantity joint_damping1 = iiwa_ros::jointQuantityFromDouble(0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8);
+	iiwa_msgs::JointQuantity joint_stiffness2 = iiwa_ros::jointQuantityFromDouble(150, 150, 150, 150, 150, 150, 150);
+//	iiwa_msgs::JointQuantity joint_damping2;
+	
+
+	impedance_configuration::ImpedanceConfiguration impedance_setter(&node_handle, "manipulator", "world");
+
+	impedance_setter.moveToInitialJointPositions();
+	impedance_setter.waitForApproval();
+	ROS_INFO_NAMED("impedance_configuration", "hi");
+	impedance_setter.setJointImpedanceMode(joint_stiffness1, joint_damping1);
+	impedance_setter.waitForApproval();
+	impedance_setter.setJointImpedanceMode(joint_stiffness2, joint_damping1);
+	impedance_setter.waitForApproval();
+	impedance_setter.setJointImpedanceMode(joint_stiffness1, joint_damping1);
+	impedance_setter.waitForApproval();
+	impedance_setter.setPositionControlMode();
+	ROS_INFO_NAMED("impedance_configuration", "bye");
 
   ros::Rate rate(10);
   while(ros::ok()) {
